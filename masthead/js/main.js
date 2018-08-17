0af3882056a27f94079209dd3a250d1b547c0799
Dewansh Rawat <dewanshrawat15@gmail.com>
2018-08-17 20:01:46 +0530

    Website Complete, bug fixes remaining

--- masthead/js/main.js
+++ masthead/js/main.js
@@ -16,4 +16,130 @@ jQuery(document).ready(function(){
 			( !$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span') ) && stretchyNavs.removeClass('nav-is-visible');
 		});
 	}
-});
+
+	setInterval(function(){
+		$('.scroll-link').addClass('neeche')
+			setTimeout(function(){
+				$('.scroll-link').removeClass('neeche')
+		}, 900)
+	}, 1800)
+
+	$(window).resize(function(){
+		var wid=$('.portfolio-img').width();
+		$('.details').css('width', ''+wid+'')
+	})
+
+	$(window).load(function(){
+		var wid=$('.portfolio-img').width();
+		$('.details').css('width', ''+wid+'')
+	})
+
+	$('.box1').hover(function(){
+		setTimeout(function(){
+			$('.box1 .stuff').toggleClass('show')
+			$('.box1 .img-init').toggleClass('img-init-active')
+		}, 500)
+	})
+
+	$('.scroll-link').click(function(){
+		$('html, body').animate({ scrollTop: $('.about').offset().top}, 1500)
+	})
+
+	$('.author').click(function(){
+		window.open("https://github.com/EDCBVUCOEP")
+	})
+
+	$("body").on("input propertychange", ".floating-label-form-group", function(e) {
+      	$(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+      	$(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+      	$(this).removeClass("floating-label-form-group-with-focus");
+    })
+
+    $("#portfolio-flters li").click(function(){
+    	$("#portfolio-flters li").removeClass('filter-active');
+    	$(this).addClass('filter-active');
+    	var selectedFilter = $(this).data("filter");
+    	$("#portfolio-wrapper").fadeTo(100, 0);
+	    $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
+    	setTimeout(function() {
+      		$(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
+      		$("#portfolio-wrapper").fadeTo(300, 1);
+    	}, 300);
+  	})
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phone = $("input#phone").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $.ajax({
+        url: "././mail/contact_me.php",
+        type: "POST",
+        data: {
+          name: name,
+          phone: phone,
+          email: email,
+          message: message
+        },
+        cache: false,
+        success: function() {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  	$("a[data-toggle=\"tab\"]").click(function(e) {
+    	e.preventDefault();
+    	$(this).tab("show");
+  	});
+
+	/*When clicking on Full hide fail/success boxes */
+	$('#name').focus(function() {
+  		$('#success').html('');
+	})
+
+})
